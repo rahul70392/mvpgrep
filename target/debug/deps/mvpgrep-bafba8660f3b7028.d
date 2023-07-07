4df635ce32378969d5b960adc4a64f694adb5a21
Rahul SIngh <703rahul.singh@gmail.com>
2023-07-07 16:12:57 +0530

    args accepted

---
+++ target/debug/deps/mvpgrep-bafba8660f3b7028.d
@@ -0,0 +1,5 @@
+/Users/rahul703/Desktop/Rust-Projects/minigrep/mvpgrep/target/debug/deps/mvpgrep-bafba8660f3b7028: src/main.rs
+
+/Users/rahul703/Desktop/Rust-Projects/minigrep/mvpgrep/target/debug/deps/mvpgrep-bafba8660f3b7028.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/mvpgrep.d
@@ -0,0 +1,1 @@
+/Users/rahul703/Desktop/Rust-Projects/minigrep/mvpgrep/target/debug/mvpgrep: /Users/rahul703/Desktop/Rust-Projects/minigrep/mvpgrep/src/main.rs

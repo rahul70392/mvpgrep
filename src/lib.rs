ee0bc7c2ed6342e56d7828300f7ec58e40ca9235
Rahul SIngh <703rahul.singh@gmail.com>
2023-07-25 15:22:23 +0530

    writing to standard err

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub struct Config {
 
 impl Config {
     pub fn build(args : &[String]) -> Result<Config, &'static str> {
-
+        
         if args.len()<3 {
             return Err("not enough arguments");
         }
